9961c24e59e5cd8ca2c4b0b9f405fdddd2acfa64
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-04-13 12:20:09 -0300

    feat(server): method post

--- api/server.js
+++ api/server.js
@@ -1,14 +1,23 @@
 require('dotenv').config();
 
 const express = require('express');
+const routes = require('./routes');
 
 const { PORT } = process.env;
 
 const app = express();
 
+app.use(express.json());
+
+const apiRoutes = express.Router();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+apiRoutes.post('/user', routes.createUser);
+
+app.use(apiRoutes);
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
