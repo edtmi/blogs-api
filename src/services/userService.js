06ff62872989045f4ee5bcc5df6ba4b6de4c274e
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-04-13 17:01:51 -0300

    feat(userService): creating user and validate if email exists

---
+++ src/services/userService.js
@@ -0,0 +1,27 @@
+const { User } = require('../models');
+const userSchema = require('../schemas/userSchema');
+
+const statusError = (status, message) => ({
+  status,
+  message,
+});
+
+const getEmail = async (email) => {
+  const result = await User.findOne({ where: { email } });
+
+  return result;
+};
+
+const create = async (userData) => {
+  const { error } = userSchema.validate(userData);
+  if (error) throw statusError(400, error.message);
+
+  if (await getEmail(userData.email)) throw statusError(409, 'User already registered');
+
+  const data = await User.create(userData);
+  return data;
+};
+
+module.exports = {
+  create,
+};
