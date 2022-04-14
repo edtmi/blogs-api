dcaf0cf02c6fc90d464581798819b4b64d77d1e6
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-04-14 05:24:46 -0300

    build(blogpost): creating model blogpost

---
+++ src/models/blogpost.js
@@ -0,0 +1,19 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const post = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  });
+
+  post.associate = (models) => {
+    post.belongsTo(models.User, 
+      { foreignKey: 'userId', as: 'user' });
+  };
+
+  return post;
+};
+
+module.exports = {
+  BlogPost,
+};
