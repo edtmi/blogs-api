b37c62d7f8880f3abcac3591c8f25e58e2572b56
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-04-13 23:29:35 -0300

    build(categoryRoutes): creating method get

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -5,6 +5,7 @@ const categoryController = require('../controllers/categoryController');
 const authorization = require('../middlewares/authorization');
 
 categoryRoute.route('/')
-  .post(authorization, categoryController.create);
+  .post(authorization, categoryController.create)
+  .get(authorization, categoryController.getCategories);
 
 module.exports = categoryRoute;
