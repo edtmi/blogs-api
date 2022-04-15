2023db28bf5c7eec6ec31fab43326eaa727beb69
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-04-14 21:00:01 -0300

    build(index): creating new route with authorization

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const routes = require('express').Router();
 const userRoutes = require('./userRoutes');
 const loginRoutes = require('./loginRoutes');
 const categoryRoutes = require('./categoryRoutes');
+const postRoutes = require('./postRoutes');
 
 const authorization = require('../middlewares/authorization');
 
 routes.use('/user', userRoutes);
 routes.use('/login', loginRoutes);
 routes.use('/categories', authorization, categoryRoutes);
+routes.use('/post', authorization, postRoutes);
 
 module.exports = routes;
