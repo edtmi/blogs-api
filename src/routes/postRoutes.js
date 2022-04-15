35cce2128163c5d1bce52b49167fb3a2fcf40495
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-04-14 22:29:28 -0300

    build(posstRoutes): creating methog get

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -3,6 +3,7 @@ const postRoute = require('express').Router();
 const postController = require('../controllers/postController');
 
 postRoute.route('/')
-  .post(postController.create);
+  .post(postController.create)
+  .get(postController.getAll);
 
 module.exports = postRoute;
