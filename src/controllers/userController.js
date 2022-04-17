cb11ba4f976529dffe45184cf3ee0cfedacfd78f
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-04-17 20:27:22 -0300

    build(userController): generating response with token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,9 +2,9 @@ const userService = require('../services/userService');
 
 const create = async (req, res, next) => {
   try {
-    const userCreated = await userService.create(req.body);
+    const token = await userService.create(req.body);
   
-    return res.status(201).json(userCreated);  
+    return res.status(201).json({ token });  
   } catch (error) {
     next(error);
   }
